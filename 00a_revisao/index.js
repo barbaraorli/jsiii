4bae3b1078a95587360bd64f6a1a849b9e45fda4
barbaraorli <barbaramsfsilva@gmail.com>
2019-10-28 13:38:25 -0300

    exercicios de revisao

--- 00a_revisao/index.js
+++ 00a_revisao/index.js
@@ -5,3 +5,19 @@
 // [] escutar o clique do botão
 // [] criar função de alterar a cor usando rgb()
 // [] usar Math.random() e Math.floor() ou parseInt()
+
+console.log('olar');
+
+const body = document.body
+const botao = document.getElementById('trocaCor')
+
+body.style.backgroundColor = 'purple'
+
+botao.addEventListener('click', () => {
+    const r = geraInteiro(255)
+    const g = geraInteiro(255)
+    const b = geraInteiro(255)
+    body.style.backgroundColor = 'rgb(' + r +',' + g + ',' + b +')'
+})
+
+geraInteiro = num => parseInt(Math.random() * num);
